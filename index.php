2852ca9b907f673175fa1dde747cca2a8f2f4d03
Furqon921 <furqon921@gmail.com>
2024-02-17 20:45:41 +0700

    manambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus ipsam cupiditate nemo, reprehenderit aspernatur enim magni ex facilis, nisi earum vero? Dolor quidem perferendis cumque accusantium? Esse similique rerum praesentium.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
